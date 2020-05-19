660fece2f970a5aba4c4514ac8bc6effcc3aeb03
Andro Mikulic <andromikulic@gmail.com>
2020-05-19 16:32:46 +0200

    Project Files

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour {
+
+	public CharacterController characterController;
+
+	[Header ("Movement")]
+	public float speed = 6.0f;
+	public float rotationSpeed = 20.0f;
+	public float gravity = 10;
+	private Vector2 moveInput = Vector2.zero;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Update () {
+		moveDirection = moveInput.x * transform.right + moveInput.y * transform.forward;
+		moveDirection *= speed;
+		moveDirection.y -= gravity;
+		characterController.Move (moveDirection * Time.deltaTime);
+	}
+
+	public void OnMove (InputValue value) {
+		moveInput = value.Get<Vector2> ();
+	}
+}
---
+++ Assets/Scripts/enemyAI.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehaviour {
+
+	[Header ("AI Agent")]
+	public NavMeshAgent ai;
+	public float fov = 94;
+	public float angle = 0;
+
+	[Header ("Player Information")]
+	public GameObject player;
+	public GameObject playerGhost;
+
+	[Header ("Visualization")]
+	public bool visualize = false;
+	public LineRenderer line;
+	public Color visibleColor;
+	public Color hiddenColor;
+
+	void Update () {
+		if (PlayerInFoV ()) {
+			playerGhost.SetActive(false);
+			playerGhost.transform.position = player.transform.position;
+			ai.destination = player.transform.position;
+		} else {
+			playerGhost.SetActive(true);
+		}
+	}
+
+	bool PlayerInFoV () {
+		bool visibility = false;
+		RaycastHit hit;
+		Vector3 rayDirection = player.transform.position - transform.position;
+		if (Physics.Raycast (transform.position, rayDirection, out hit, Mathf.Infinity)) {
+			if (hit.transform.tag.Equals (player.tag)) {
+				angle = Vector3.Angle (transform.forward, rayDirection);
+				if (angle <= fov) {
+					visibility = true;
+				}
+			}
+		}
+		Visualize (visibility);
+		return visibility;
+	}
+
+	void Visualize (bool visible) {
+		if (!visualize) {
+			line.positionCount = 0;
+			return;
+		}
+		if (visible) {
+			line.material.color = visibleColor;
+		} else {
+			line.material.color = hiddenColor;
+		}
+		Vector3[] positions = { transform.position, player.transform.position };
+		line.positionCount = positions.Length;
+		line.SetPositions (positions);
+	}
+}
